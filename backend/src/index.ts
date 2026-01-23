86a65c2bc7f27a380f6b1694457a67688942c096
charliewturner <c.w.turner@hotmail.com>
2026-01-23 17:19:05 +0000

    Work on API linking

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,6 +11,29 @@ app.get("/health", (_req, res) => {
   res.json({ ok: true });
 });
 
+app.get("/courses/search", async (_req, res) => {
+  try {
+    const response = await fetch("https://api.golfcourseapi.com/v1/search", {
+      headers: {
+        Authorization: `Key ${process.env.API_KEY}`,
+      },
+    });
+
+    if (!response.ok) {
+      const text = await response.text();
+      return res.status(response.status).json({
+        error: "GolfCourseAPI error",
+        details: text,
+      });
+    }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Server error", err });
+  }
+});
+
 const PORT = process.env.PORT ? Number(process.env.PORT) : 8000;
 app.listen(PORT, () => {
   console.log(`API running on http://localhost:${PORT}`);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,32 @@ import { useEffect, useState } from "react";
 export default function App() {
   const [status, setStatus] = useState<string>("loading...");
 
-  useEffect(() => {
-    fetch("http://localhost:8000/health")
-      .then((r) => r.json())
-      .then((d) => setStatus(d.ok ? "API OK" : "API not ok"))
-      .catch(() => setStatus("API error"));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/health")
+  //     .then((r) => r.json())
+  //     .then((d) => {
+  //       console.log("Health response:", d);
+  //       setStatus(d.ok ? "API OK" : "API not ok");
+  //     })
 
+  //     .catch(() => setStatus("API error"));
+  // }, []);
+
+  useEffect(() => {
+    fetch("https://api.golfcourseapi.com/v1/search", {
+      headers: {
+        Authorization: `Key ${import.meta.env.API_KEY}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
-    <div style={{ padding: 16 }}>
-      <h1>Golf Tracker</h1>
-      <p>{status}</p>
-    </div>
+    <>
+      <div style={{ padding: 16 }}>
+        <h1>Golf Tracker</h1>
+        <p>API STATUS: {status}</p>
+      </div>
+    </>
   );
 }
